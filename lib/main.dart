6228fe35b4b3bd335b050cc7de1cac1057238104
Josh <joshg1221@protonmail.com>
2024-03-07 16:47:48 -0800

    Add Dark Theme + Button to Switch Between Light & Dark

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Calculator(),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: const Calculator(),
     );
   }
 }
 
 class Calculator extends StatefulWidget {
+  const Calculator({Key? key});
+
   @override
   _CalculatorState createState() => _CalculatorState();
 }
@@ -20,11 +26,12 @@ class Calculator extends StatefulWidget {
 class _CalculatorState extends State<Calculator> {
   String _output = "0";
   String _outputFull = "";
-  List<String> _history = [];
+  final List<String> _history = [];
   String _operand = "";
   double _num1 = 0.0;
   double _num2 = 0.0;
   final int _inputLimit = 10;
+  bool _isDarkMode = false;
 
   buttonPressed(String buttonText) {
     if (buttonText == "CLEAR") {
@@ -70,7 +77,7 @@ class _CalculatorState extends State<Calculator> {
       if (_history.length == 1) {
         _history.removeAt(0);
       }
-      _history.add(_outputFull + "=" + _output);
+      _history.add("$_outputFull=$_output");
       _outputFull = _output;
     } else {
       if (_output.length >= _inputLimit) {
@@ -89,11 +96,11 @@ class _CalculatorState extends State<Calculator> {
   Widget buildButton(String buttonText, Color buttonColor, Color textColor) {
     return Expanded(
       child: Container(
-        margin: EdgeInsets.all(8.0),
+        margin: const EdgeInsets.all(8.0),
         child: ElevatedButton(
           style: ElevatedButton.styleFrom(
             backgroundColor: buttonColor,
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 0),
+            padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 0),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(18.0),
             ),
@@ -111,77 +118,94 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Basic Calculator"),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(20),
-              alignment: Alignment.bottomRight,
-              child: Text(
-                _outputFull,
-                style: TextStyle(fontSize: 24, color: Colors.grey),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(20),
-              alignment: Alignment.bottomRight,
-              child: Text(
-                _output,
-                style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Divider(height: 1),
-            Column(children: [
-              for (var i = 0; i < _history.length; i++)
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    _history[_history.length - 1 - i],
-                    style: TextStyle(fontSize: 18, color: Colors.blueGrey),
-                  ),
-                ),
-            ]),
-            Container(
-              child: Column(
-                children: [
-                  Row(children: [
-                    buildButton("7", Colors.blueGrey[50]!, Colors.black),
-                    buildButton("8", Colors.blueGrey[50]!, Colors.black),
-                    buildButton("9", Colors.blueGrey[50]!, Colors.black),
-                    buildButton("/", Colors.blueGrey, Colors.white),
-                  ]),
-                  Row(children: [
-                    buildButton("4", Colors.blueGrey[50]!, Colors.black),
-                    buildButton("5", Colors.blueGrey[50]!, Colors.black),
-                    buildButton("6", Colors.blueGrey[50]!, Colors.black),
-                    buildButton("*", Colors.blueGrey, Colors.white),
-                  ]),
-                  Row(children: [
-                    buildButton("1", Colors.blueGrey[50]!, Colors.black),
-                    buildButton("2", Colors.blueGrey[50]!, Colors.black),
-                    buildButton("3", Colors.blueGrey[50]!, Colors.black),
-                    buildButton("-", Colors.blueGrey, Colors.white),
-                  ]),
-                  Row(children: [
-                    buildButton(".", Colors.blueGrey[50]!, Colors.black),
-                    buildButton("0", Colors.blueGrey[50]!, Colors.black),
-                    buildButton("00", Colors.blueGrey[50]!, Colors.black),
-                    buildButton("+", Colors.blueGrey, Colors.white),
-                  ]),
-                  Row(children: [
-                    buildButton("CLEAR", Colors.redAccent, Colors.white),
-                    buildButton("=", Colors.green, Colors.white),
-                  ]),
-                ],
-              ),
+    return MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: _isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Basic Calculator"),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: Icon(_isDarkMode ? Icons.light_mode : Icons.dark_mode),
+              onPressed: () {
+                setState(() {
+                  _isDarkMode = !_isDarkMode;
+                });
+              },
             ),
           ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(20),
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  _outputFull,
+                  style: const TextStyle(fontSize: 24, color: Colors.grey),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(20),
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  _output,
+                  style: const TextStyle(
+                      fontSize: 48, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const Divider(height: 1),
+              Column(children: [
+                for (var i = 0; i < _history.length; i++)
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      _history[_history.length - 1 - i],
+                      style:
+                          const TextStyle(fontSize: 18, color: Colors.blueGrey),
+                    ),
+                  ),
+              ]),
+              Container(
+                child: Column(
+                  children: [
+                    Row(children: [
+                      buildButton("7", Colors.blueGrey[50]!, Colors.black),
+                      buildButton("8", Colors.blueGrey[50]!, Colors.black),
+                      buildButton("9", Colors.blueGrey[50]!, Colors.black),
+                      buildButton("/", Colors.blueGrey, Colors.white),
+                    ]),
+                    Row(children: [
+                      buildButton("4", Colors.blueGrey[50]!, Colors.black),
+                      buildButton("5", Colors.blueGrey[50]!, Colors.black),
+                      buildButton("6", Colors.blueGrey[50]!, Colors.black),
+                      buildButton("*", Colors.blueGrey, Colors.white),
+                    ]),
+                    Row(children: [
+                      buildButton("1", Colors.blueGrey[50]!, Colors.black),
+                      buildButton("2", Colors.blueGrey[50]!, Colors.black),
+                      buildButton("3", Colors.blueGrey[50]!, Colors.black),
+                      buildButton("-", Colors.blueGrey, Colors.white),
+                    ]),
+                    Row(children: [
+                      buildButton(".", Colors.blueGrey[50]!, Colors.black),
+                      buildButton("0", Colors.blueGrey[50]!, Colors.black),
+                      buildButton("00", Colors.blueGrey[50]!, Colors.black),
+                      buildButton("+", Colors.blueGrey, Colors.white),
+                    ]),
+                    Row(children: [
+                      buildButton("CLEAR", Colors.redAccent, Colors.white),
+                      buildButton("=", Colors.green, Colors.white),
+                    ]),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
